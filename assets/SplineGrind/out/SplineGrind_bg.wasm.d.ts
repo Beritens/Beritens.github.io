77959131c0139f4e7ea2d3fc390e18e92358de4b
Ben <richterneb@gmail.com>
2025-08-21 13:04:11 +0200

    spline grind

---
+++ assets/SplineGrind/out/SplineGrind_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hbc5f4cdfe79b4eb1: (a: number, b: number, c: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h03a83162dcb14fcc: (a: number, b: number) => void;
+export const closure5025_externref_shim: (a: number, b: number, c: any) => void;
+export const closure5032_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__he8a74ec356e71e21: (a: number, b: number) => void;
+export const closure118685_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
